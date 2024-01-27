7253d2a736b4f357fcf0c3f3182214c6da256d5b
Mahalakshmi <mmahaece07@gmail.com>
2024-01-27 08:34:52 +0530

    Search created

---
+++ search/src/Search.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import  "./search.css";
+import axios from 'axios';
+
+function Search()
+{
+    const [countries, setCountries] = useState([]);
+    const [search, setSearch] = useState('');
+
+    const fetchdata = async() => {
+        try{
+        let result = await axios.get(`https://restcountries.com/v3.1/all`);
+       setCountries(result.data);
+       setSearch('');
+        }
+        catch(e)
+        {
+            console.log("Error fetching data:", e);
+        }
+    }
+    useEffect(() => {
+    fetchdata();
+    }, [])
+
+    const filteredCountries = countries.filter((country) =>
+    country.name.common && country.name.common.includes(search)
+  );
+
+  return(
+    <div className='box'>
+    <input type='text' className='search' value={search} onChange={(e) => setSearch(e.target.value)} 
+    placeholder='Search for  countries'
+    />
+    <div className="container">
+    {search === '' ? (
+    countries.map((country, idx) => (
+    <div className="card" key={idx}>
+      <img src={country.flags.png} alt={`img of ${country.name.common}`}  className="img"/>
+      <h2>{country.name.common}</h2>
+    </div>
+  ))
+    ) : (
+        filteredCountries.map((cn, idx) => (
+     <div className="card" key={idx}>
+      <img src={cn.flags.png} alt={`img of ${cn.name.common}`}  className="img"/>
+      <h2>{cn.name.common}</h2>
+    </div>
+        ))
+    )}
+  </div>
+  </div>
+  );
+}
+export default Search;
